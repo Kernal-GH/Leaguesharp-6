0f68bb1e60eb3cd6d34a45c788503192dd8cb4de
Nouser <nouser@toinfinitygaming.com>
2015-04-28 22:20:19 -0400

    Changed draw position to make it more readable. Menu is there for future implementation of choosing flashing loading or increasing numbers.

--- LeagueSharp/NoMoreFreezes/NoMoreFreezes/Program.cs
+++ LeagueSharp/NoMoreFreezes/NoMoreFreezes/Program.cs
@@ -12,23 +12,44 @@ namespace NoMoreFreezes
     {
         private static int counter = 0;
         private static bool InGame = false;
+        private static string[] options = {"Flashing Loading", "Increasing Number"};
+        private static Menu mainMenu = new Menu("NoMoreFreezes", "NoMoreFreezes", true);
+        private static bool number = true;
         static void Main(string[] args)
         {
-            Game.OnStart += GameStarted;
-            CustomEvents.Game.OnGameLoad += GameStarted;
+            Game.OnStart += OnGameLoad;
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+            Game.OnUpdate += OnUpdate;
             Drawing.OnDraw += DrawNotFrozen;
+            mainMenu.AddItem(new MenuItem("Style:", "Style:", false).SetValue(new StringList(options, 0)));
+            mainMenu.AddToMainMenu();
+            if (mainMenu.Item("Style:").GetValue<string>().Equals("Increasing Number"))
+                number = false;
         }
 
         private static void DrawNotFrozen(EventArgs args)
         {
-            if (!InGame)
-               
+            if (InGame)
+            return;
+            else
             {
-                Drawing.DrawText(100,100, Color.Red, "You are still in game if this is increasing:" + counter);
+               if(number)
+                Drawing.DrawText(10,10, Color.Red, "You are still in game if this is increasing:" + counter);
+                else
+                {
+                    Color temp = ((int)Math.Truncate((double)counter/2)%2==0) ? Color.White : Color.Green;
+                    Drawing.DrawText(10, 10, temp, "Loading");
+                }
                 counter++;
             }
         }
-        private static void GameStarted(EventArgs args)
+
+        private static void OnUpdate(EventArgs args)
+        {
+            if (Game.Time > 0)
+                InGame = true;
+        }
+        private static void OnGameLoad(EventArgs args)
         {
             InGame = true;
         }
