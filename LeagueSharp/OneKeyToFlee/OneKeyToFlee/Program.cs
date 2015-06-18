df71da0699d4e6eea45a9465169c585186a4b31b
Nouser <nouser@toinfinitygaming.com>
2015-06-17 20:58:46 -0400

    Initial commit for OKTF.

---
+++ LeagueSharp/OneKeyToFlee/OneKeyToFlee/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using OneKeyToFlee.Plugins;
+
+
+namespace OneKeyToFlee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        public static void OnGameLoad(EventArgs args)
+        {
+            try
+            {
+                switch (ObjectManager.Player.ChampionName)
+                {
+                    case "Azir":
+                        Azir.Load();
+                        break;
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
