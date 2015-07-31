139d58164e2630d6cb7ea91fb71ed07a961ab834
Nouser <nouser@toinfinitygaming.com>
2015-07-31 18:59:51 -0400

    Formatted for StyleCop, added a fake move click after autoattack.

--- LeagueSharp/StreamSharp/StreamSharp/Program.cs
+++ LeagueSharp/StreamSharp/StreamSharp/Program.cs
@@ -1,31 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization.Configuration;
-using LeagueSharp;
-using SharpDX;
-using LeagueSharp.Common;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Nouser">
+//   Copyright Me
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace StreamSharp
 {
-    class Program
+    using System;
+    using System.Linq;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    using SharpDX;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
     {
-        static Menu root = new Menu("Stream", "Stream", true);
-        private static float deltaT = .2f;
-        private static Vector3 lastEndpoint = new Vector3();
-        private static float lastTime = 0f;
+        #region Static Fields
+
+        /// <summary>
+        /// If the user is attacking
+        /// Currently used for the second style of fake clicks
+        /// </summary>
         private static bool attacking = false;
+
+        /// <summary>
+        /// The delta t for click frequency
+        /// </summary>
+        private static float deltaT = .2f;
+
+        /// <summary>
+        /// The last direction of the player
+        /// </summary>
+        private static Vector3 direction;
+
+        /// <summary>
+        /// The last endpoint the player was moving to.
+        /// </summary>
+        private static Vector3 lastEndpoint = new Vector3();
+
+        /// <summary>
+        /// The last order the player had.
+        /// </summary>
+        private static GameObjectOrder lastOrder;
+
+        /// <summary>
+        /// The time of the last order the player had.
+        /// </summary>
         private static float lastOrderTime = 0f;
-        static void Main(string[] args)
+
+        /// <summary>
+        /// The last time a click was done.
+        /// </summary>
+        private static float lastTime = 0f;
+
+        /// <summary>
+        /// The Player.
+        /// </summary>
+        private static Obj_AI_Hero player;
+
+        /// <summary>
+        /// The Random number generator
+        /// </summary>
+        private static Random r = new Random();
+
+        /// <summary>
+        /// The root menu.
+        /// </summary>
+        private static Menu root = new Menu("Stream", "Stream", true);
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The move fake click after attacking
+        /// </summary>
+        /// <param name="unit">
+        /// The unit.
+        /// </param>
+        /// <param name="target">
+        /// The target.
+        /// </param>
+        private static void AfterAttack(AttackableUnit unit, AttackableUnit target)
         {
-            CustomEvents.Game.OnGameLoad += OnLoad;
-            Game.OnUpdate += OnUpdate;
-            Hacks.DisableDrawings = true;
-            Hacks.DisableSay = true;
-            Hacks.PingHack = false;
+            attacking = false;
+            var t = target as Obj_AI_Hero;
+            if (t != null && unit.IsMe)
+            {
+                Hud.ShowClick(ClickType.Move, RandomizePosition(t.Position));
+            }
         }
 
+        /// <summary>
+        /// The angle between two vectors.
+        /// </summary>
+        /// <param name="a">
+        /// The first vector.
+        /// </param>
+        /// <param name="b">
+        /// The second vector.
+        /// </param>
+        /// <returns>
+        /// The Angle between two vectors
+        /// </returns>
+        private static float AngleBetween(Vector3 a, Vector3 b)
+        {
+            var dotProd = Vector3.Dot(a, b);
+            var lenProd = a.Length() * b.Length();
+            var divOperation = dotProd / lenProd;
+            return (float)(Math.Acos(divOperation) * (180.0 / Math.PI));
+        }
+
+        /// <summary>
+        /// The before attack fake click.
+        /// Currently used for the second style of fake clicks
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
         private static void BeforeAttackFake(Orbwalking.BeforeAttackEventArgs args)
         {
             if (root.SubMenu("Fake Clicks").Item("Click Mode").GetValue<StringList>().SelectedIndex == 1)
@@ -35,82 +134,144 @@ namespace StreamSharp
             }
         }
 
-        private static void OnIssueOrder(Obj_AI_Base sender, GameObjectIssueOrderEventArgs args)
-        {
-            if (sender.IsMe && (args.Order == GameObjectOrder.MoveTo || args.Order == GameObjectOrder.AttackUnit || args.Order == GameObjectOrder.AttackTo) 
-                && lastOrderTime + (deltaT) < Game.Time && root.SubMenu("Fake Clicks").Item("Enable").IsActive() 
-                && root.SubMenu("Fake Clicks").Item("Click Mode").GetValue<StringList>().SelectedIndex==0)
-            {
-                Vector3 vect = args.TargetPosition;
-                vect.Z = ObjectManager.Player.Position.Z;
-                if (args.Order == GameObjectOrder.AttackUnit || args.Order == GameObjectOrder.AttackTo)
-                    Hud.ShowClick(ClickType.Attack, RandomizePosition(vect));
-                else
-                    Hud.ShowClick(ClickType.Move, vect);
-                lastOrderTime = Game.Time;
-            }
-        }
-        private static void AfterAttack(AttackableUnit atk, AttackableUnit atk2)
-        {
-            attacking = false;
-        }
-
+        /// <summary>
+        /// The fake click before you cast a spell
+        /// </summary>
+        /// <param name="s">
+        /// The spellbook.
+        /// </param>
+        /// <param name="args">
+        /// The args.
+        /// </param>
         private static void BeforeSpellCast(Spellbook s, SpellbookCastSpellEventArgs args)
         {
-            if(args.Target.Position.Distance(ObjectManager.Player.Position)>=5f)
-            Hud.ShowClick(ClickType.Attack, args.Target.Position);
+            if (args.Target.Position.Distance(player.Position) >= 5f)
+            {
+                Hud.ShowClick(ClickType.Attack, args.Target.Position);
+            }
         }
 
+        /// <summary>
+        /// The on new path fake.
+        /// Currently used for the second style of fake clicks
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="args">
+        /// The args.
+        /// </param>
         private static void DrawFake(Obj_AI_Base sender, GameObjectNewPathEventArgs args)
         {
-            if (sender.IsMe && lastTime + deltaT < Game.Time && args.Path.LastOrDefault() != lastEndpoint && args.Path.LastOrDefault().Distance(ObjectManager.Player.ServerPosition) >=5f && root.SubMenu("Fake Clicks").Item("Enable").IsActive()
+            if (sender.IsMe && lastTime + deltaT < Game.Time && args.Path.LastOrDefault() != lastEndpoint
+                && args.Path.LastOrDefault().Distance(player.ServerPosition) >= 5f
+                && root.SubMenu("Fake Clicks").Item("Enable").IsActive()
                 && root.SubMenu("Fake Clicks").Item("Click Mode").GetValue<StringList>().SelectedIndex == 1)
             {
                 lastEndpoint = args.Path.LastOrDefault();
                 if (!attacking)
+                {
                     Hud.ShowClick(ClickType.Move, Game.CursorPos);
+                }
                 else
+                {
                     Hud.ShowClick(ClickType.Attack, Game.CursorPos);
+                }
+
                 lastTime = Game.Time;
             }
         }
 
-        static Vector3 RandomizePosition(Vector3 input)
+        /// <summary>
+        /// The main entry point.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
         {
-            Random r = new Random((int)input.X);
-            if (r.Next(2) == 0)
-                input.X += r.Next(100);
-            else
-                input.Y += r.Next(100);
-            return input;
+            CustomEvents.Game.OnGameLoad += OnLoad;
+            Game.OnUpdate += OnUpdate;
+            Hacks.DisableDrawings = true;
+            Hacks.DisableSay = true;
+            Hacks.PingHack = false;
         }
 
-        static void OnLoad(EventArgs args)
+        /// <summary>
+        /// The OnIssueOrder event delegate.
+        /// Currently used for the first style of fake clicks
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void OnIssueOrder(Obj_AI_Base sender, GameObjectIssueOrderEventArgs args)
+        {
+            if (sender.IsMe
+                && (args.Order == GameObjectOrder.MoveTo || args.Order == GameObjectOrder.AttackUnit
+                    || args.Order == GameObjectOrder.AttackTo)
+                && lastOrderTime + r.NextFloat(deltaT, deltaT + .2f) < Game.Time
+                && root.SubMenu("Fake Clicks").Item("Enable").IsActive()
+                && root.SubMenu("Fake Clicks").Item("Click Mode").GetValue<StringList>().SelectedIndex == 0)
+            {
+                var vect = args.TargetPosition;
+                vect.Z = player.Position.Z;
+                if (args.Order == GameObjectOrder.AttackUnit || args.Order == GameObjectOrder.AttackTo)
+                {
+                    Hud.ShowClick(ClickType.Attack, RandomizePosition(vect));
+                }
+                else
+                {
+                    Hud.ShowClick(ClickType.Move, vect);
+                }
+
+                lastOrderTime = Game.Time;
+            }
+        }
+
+        /// <summary>
+        /// The OnLoad event delegate
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void OnLoad(EventArgs args)
         {
             root.AddItem(new MenuItem("Drawings", "Drawings").SetValue(new KeyBind('H', KeyBindType.Toggle, true)));
             root.AddItem(new MenuItem("Game Say", "Game Say").SetValue(new KeyBind('K', KeyBindType.Toggle, false)));
             root.AddItem(new MenuItem("Stream", "Stream").SetValue(new KeyBind('L', KeyBindType.Toggle, false)));
             root.AddItem(new MenuItem("Config", "Config").SetValue(new KeyBind(';', KeyBindType.Toggle, false)));
 
-            Menu fakeClickMenu = new Menu("Fake Clicks", "Fake Clicks", false);
+            var fakeClickMenu = new Menu("Fake Clicks", "Fake Clicks", false);
             fakeClickMenu.AddItem(new MenuItem("Enable", "Enable").SetValue(true));
-            fakeClickMenu.AddItem(new MenuItem("Click Mode", "Click Mode")).SetValue(new StringList(new string[] {"Evade, No Cursor Position", "Cursor Position, No Evade"}));
+            fakeClickMenu.AddItem(new MenuItem("Click Mode", "Click Mode"))
+                .SetValue(new StringList(new[] { "Evade, No Cursor Position", "Cursor Position, No Evade" }));
             root.AddSubMenu(fakeClickMenu);
             root.AddToMainMenu();
 
-            Obj_AI_Hero.OnNewPath += DrawFake;
+            player = ObjectManager.Player;
+
+            Obj_AI_Base.OnNewPath += DrawFake;
             Orbwalking.BeforeAttack += BeforeAttackFake;
             Spellbook.OnCastSpell += BeforeSpellCast;
             Orbwalking.AfterAttack += AfterAttack;
             Obj_AI_Base.OnIssueOrder += OnIssueOrder;
         }
 
-        static void OnUpdate(EventArgs args)
+        /// <summary>
+        /// The OnUpdate event delegate.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void OnUpdate(EventArgs args)
         {
             if (!root.Item("Stream").IsActive() && !root.Item("Config").IsActive())
             {
-                Hacks.DisableDrawings = !(root.Item("Drawings").IsActive());
-                Hacks.DisableSay = (root.Item("Game Say").IsActive());
+                Hacks.DisableDrawings = !root.Item("Drawings").IsActive();
+                Hacks.DisableSay = root.Item("Game Say").IsActive();
             }
             else if (root.Item("Config").IsActive())
             {
@@ -123,5 +284,30 @@ namespace StreamSharp
                 Hacks.DisableSay = true;
             }
         }
+
+        /// <summary>
+        /// The RandomizePosition function to randomize click location.
+        /// </summary>
+        /// <param name="input">
+        /// The input Vector3.
+        /// </param>
+        /// <returns>
+        /// A Vector within 100 units of the unit
+        /// </returns>
+        private static Vector3 RandomizePosition(Vector3 input)
+        {
+            if (r.Next(2) == 0)
+            {
+                input.X += r.Next(100);
+            }
+            else
+            {
+                input.Y += r.Next(100);
+            }
+
+            return input;
+        }
+
+        #endregion
     }
 }
