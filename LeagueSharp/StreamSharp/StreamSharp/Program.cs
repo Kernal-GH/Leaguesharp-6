311df576bd8b3d7dc4d4fb1b498d8b96c1a0cdfa
Nouser <nouser@toinfinitygaming.com>
2015-07-30 23:11:52 -0400

    Updated Default Menu Values

--- LeagueSharp/StreamSharp/StreamSharp/Program.cs
+++ LeagueSharp/StreamSharp/StreamSharp/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization.Configuration;
 using LeagueSharp;
+using SharpDX;
 using LeagueSharp.Common;
 namespace StreamSharp
 {
@@ -22,10 +23,10 @@ namespace StreamSharp
 
         static void OnLoad(EventArgs args)
         {
-            root.AddItem(new MenuItem("Drawings", "Drawings").SetValue(new KeyBind('H', KeyBindType.Toggle, false)));
-            root.AddItem(new MenuItem("Zoom", "Zoom").SetValue(new KeyBind('J', KeyBindType.Toggle, true)));
-            root.AddItem(new MenuItem("Game Say", "Game Say").SetValue(new KeyBind('K', KeyBindType.Toggle, true)));
-            root.AddItem(new MenuItem("Stream", "Stream").SetValue(new KeyBind('L', KeyBindType.Toggle, true)));
+            root.AddItem(new MenuItem("Drawings", "Drawings").SetValue(new KeyBind('H', KeyBindType.Toggle, true)));
+            root.AddItem(new MenuItem("Zoom", "Zoom").SetValue(new KeyBind('J', KeyBindType.Toggle, false)));
+            root.AddItem(new MenuItem("Game Say", "Game Say").SetValue(new KeyBind('K', KeyBindType.Toggle, false)));
+            root.AddItem(new MenuItem("Stream", "Stream").SetValue(new KeyBind('L', KeyBindType.Toggle, false)));
             root.AddItem(new MenuItem("Config", "Config").SetValue(new KeyBind(';', KeyBindType.Toggle, false)));
             root.AddToMainMenu();
         }
